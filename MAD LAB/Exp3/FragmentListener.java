535c71d737641aef05502a38d08c335d92f13553
himabindu1226 <60434430+himabindu1226@users.noreply.github.com>
2021-08-05 17:42:01 +0530

    Add files via upload

---
+++ MAD LAB/Exp3/FragmentListener.java
@@ -0,0 +1,7 @@
+package com.example.myapplication1;
+
+public class FragmentListener {
+    String KEY_SELECTED_CANDIDATE="KEY_SELECTED_CANDIDATE";
+
+    void onCandidateSelected(String candidate);
+}
